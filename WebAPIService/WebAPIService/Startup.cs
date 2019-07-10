ecb86fd302a1d1d04e164f88c829105091dea31f
Sai babu Govindu <govindu84>
2019-07-10 16:34:17 -0700

    Entity Framework in memory todo controller

--- WebAPIService/WebAPIService/Startup.cs
+++ WebAPIService/WebAPIService/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAPIService
 {
@@ -15,7 +16,9 @@ namespace WebAPIService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddXmlDataContractSerializerFormatters();
+            services.AddDbContext<TodoDbContext>(options => options.UseInMemoryDatabase("foo"));
+            services.AddMvc()
+                .AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
